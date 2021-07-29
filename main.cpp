3e87cb9f570431079b8c7ff740e43c73c089264b
Plakhtiev Jaroslav <63554869+Plakhtiev@users.noreply.github.com>
2021-07-29 20:51:13 +0300

    pthreadExemple

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
-void* tfunc(void* param) {
+#include <iostream>
+#include <pthread.h>
 
-std::cout << param << std::endl;
-return nullptr;
+#include <QCoreApplication>
+#include <QTextStream>
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void* ShowThread(void* param) {
+    int* i = static_cast<int*>(param);
+    pthread_mutex_lock(&lock);
+
+    std::cout<<"I am thread  " << *i<< std::endl;
+
+    pthread_mutex_unlock(&lock);
+    return nullptr;
 }
+
 
 int main()
 {
-pthread_t tid = 0;
-pthread_attr_t attr = {};
-pthread_attr_init(&attr);
 
-pthread_create(&tid, &attr, tfunc, 0);
-sleep(1);
+    pthread_t tid[2] = {};
+    pthread_attr_t attr = {};
 
-void* data;
-pthread_join(tid, &data);
-cout << "Hello World!" << endl;
-return 0;
-} 
+    for(int i = 0; i < 2; ++i) {
+        pthread_create(&tid[i], &attr, ShowThread, &i);
+
+    }
+    void* data;
+    pthread_join(tid[0], &data);
+    pthread_join(tid[1], &data);
+}
